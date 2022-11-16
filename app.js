71338dc98236f4efe22b599fcabf9f2812120bab
Vivek kasture <vivek_kasture@techjoomla.com>
2022-11-16 13:23:49 +0000

    Added script to run

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 
 const app = express();
 
-
 app.get('/', (req, res) => {
   res.send("<h1>Hello there! Server is working");
 });
--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const app = require('./app');
 const { PORT } = process.env;
 
 app.listen(PORT, () => {
+  
   console.log(`server is running on port ${PORT}`);
 });
